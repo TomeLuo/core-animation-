7c9eb9ae805b98576524c6f7676e00d6c3324d8c
童川 <tongchuan@tongchuandeimac.local>
2016-01-10 23:17:18 -0800

    Initial Commit

---
+++ core animation动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  core animation动画
+//
+//  Created by 童川 on 16/1/10.
+//  Copyright © 2016年 童川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ core animation动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  core animation动画
+//
+//  Created by 童川 on 16/1/10.
+//  Copyright © 2016年 童川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
